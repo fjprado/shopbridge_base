c0f2093b10c10b1433a2bd91d2a24aa28f067a15
FernandoPrado <dev.fprado@gmail.com>
2023-03-20 21:51:02 -0300

    Changed Repository and created base and product repositories

---
+++ Data/Repository/Implementations/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shopbridge_base.Data.Repository.Interfaces;
+using Shopbridge_base.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Shopbridge_base.Data.Repository.Implementations
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public ProductRepository(Shopbridge_Context dbcontext) : base(dbcontext)
+        {
+        }
+    }
+}
---
+++ Data/Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using Shopbridge_base.Domain.Models;
+
+namespace Shopbridge_base.Data.Repository.Interfaces
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+    }
+}
