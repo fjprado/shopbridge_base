3e8d52e25e8ecf369fe15003cbf515b911dea7bd
FernandoPrado <dev.fprado@gmail.com>
2023-03-20 21:50:16 -0300

    Setup Product Configuration

---
+++ Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Shopbridge_base.Domain.Models;
+
+namespace Shopbridge_base.Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Product_Id);
+
+            builder.Property(p => p.Product_Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Product_Description);
+
+            builder.Property(p => p.Product_Price)
+                .IsRequired()
+                .HasPrecision(2);
+
+            builder.Property(p => p.Product_StockBalance)
+                .IsRequired()
+                .HasPrecision(4);
+        }
+    }
+}
